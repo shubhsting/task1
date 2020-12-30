6e4e16c60059e5bf8517f69721565facc54d3f3c
Shubham <shubhamsingh1840@gmail.com>
2020-12-30 17:02:38 +0530

    updated

--- user.js
+++ user.js
@@ -1,39 +1,39 @@
 const axios = require("axios");
 
 
-
-// axios.post('http://localhost:3000/add', {
-//     body: {
-//         key: 12345,
-//         time: 100
-//     }
-// })
-//     .then(function (response) {
-//         console.log(response.data);
-//     })
-//     .catch(function (error) {
-//         console.log(error);
-//     })
-
-
-
-// axios.get('http://localhost:3000/read/key-name', {
-// })
-//     .then(function (response) {
-//         console.log(response.data);
-//     })
-//     .catch(function (error) {
-//         console.log(error);
-//     })
+//sample api request to add an element
+axios.post('http://localhost:3000/add', {
+    body: {
+        key: 12345,
+        time: 100
+    }
+})
+    .then(function (response) {
+        console.log(response.data);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
 
 
+//sample api request to get a key from keyname specified in url in place of key-name
+axios.get('http://localhost:3000/read/key-name', {
+})
+    .then(function (response) {
+        console.log(response.data);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
 
-// axios.delete('http://localhost:3000/delete/key-name', {
 
-// })
-//     .then(function (response) {
-//         console.log(response.data);
-//     })
-//     .catch(function (error) {
-//         console.log(error);
-//     })
+//sample api request to delete a key from keyname specified in url in place of key-name
+axios.delete('http://localhost:3000/delete/key-name', {
+
+})
+    .then(function (response) {
+        console.log(response.data);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
